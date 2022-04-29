11a3708b62a476ad4a7bae51a34a6832a24d40d8
jazzg62 <714508719@qq.com>
2022-04-29 11:31:35 +0800

    修复临时文件夹可能会过大的问题

--- extension.js
+++ extension.js
@@ -3,26 +3,27 @@
 const vscode = require('vscode');
 const cgcf = require('cgcf');
 const path = require('path');
+const fs = require('fs');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
 const workspaceRoot = vscode.workspace.rootPath;
-const tmp = '.git/'+(Math.random()*100000).toFixed(0);
+const tmp = '.git/cc.changes';
 const target_path = path.join(workspaceRoot,tmp);
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	let copyChanges = vscode.commands.registerCommand('cgcf.copyChanges', async function(){
+	let copyChanges = vscode.commands.registerCommand('cc.copyChanges', async function(){
 		cgcf.clear(target_path);
         // 提示正在读取拷贝文件
-		vscode.window.setStatusBarMessage('正在读取拷贝文件...');
+		vscode.window.setStatusBarMessage('正在读取改动文件...');
         let res = await cgcf.getGitRepoChanges(workspaceRoot);
 		// 无文件改动
 		if(res.length === 0){
-			vscode.window.showInformationMessage('no changes');
+			vscode.window.showInformationMessage('无文件改动！');
 			return ;
 		}
         let source_file = "", target_file = "";
@@ -33,10 +34,10 @@ function activate(context) {
             cgcf.copy(source_file, target_file);
         }
 		vscode.window.setStatusBarMessage(`拷贝成功！共${res.length}个项目`);
-		cgcf.openInExplorer(target_path);
 		setTimeout(()=>{
 			vscode.window.setStatusBarMessage(null);
 		},2000);
+		cgcf.openInExplorer(target_path);
 	})
 
 	context.subscriptions.push(copyChanges);
@@ -44,7 +45,8 @@ function activate(context) {
 
 // this method is called when your extension is deactivated
 function deactivate() {
-	cgcf.clear(target_path);
+	if(fs.existsSync(target_path))
+		cgcf.clear(target_path);
 }
 
 module.exports = {
